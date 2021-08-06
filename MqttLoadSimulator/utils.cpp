85076c46df58d5252f1fd317c82097f85bf18457
Wiebe Cazemier <wiebe@halfgaar.net>
2021-08-06 14:52:07 +0200

    Create safe random client id
    When I'll be starting parallel processes, they may all seed the same,
    which would cause conflicting clientid's, which kicks out clients.

--- MqttLoadSimulator/utils.cpp
+++ MqttLoadSimulator/utils.cpp
@@ -1,14 +1,26 @@
 #include "utils.h"
 
+#include "sys/random.h"
+
+#include "vector"
+
 QString GetRandomString()
 {
    const QString possibleCharacters("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
    const int randomStringLength = 12; // assuming you want random strings of 12 characters
 
-   QString randomString;
-   for(int i=0; i<randomStringLength; ++i)
+   std::vector<unsigned char> buf(randomStringLength);
+   ssize_t actual_len = getrandom(buf.data(), randomStringLength, 0);
+
+   if (actual_len < 0 || actual_len != randomStringLength)
    {
-       int index = qrand() % possibleCharacters.length();
+       throw std::runtime_error("Error requesting random data");
+   }
+
+   QString randomString;
+   for(const unsigned char c : buf)
+   {
+       const int index = c % possibleCharacters.length();
        QChar nextChar = possibleCharacters.at(index);
        randomString.append(nextChar);
    }
